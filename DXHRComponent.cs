807bc792e4c84d134d6ec2b51f98bec873a814d9
Ian Robinson <ian@chango.com>
2014-11-27 21:02:45 -0500

    Initial commit

---
+++ DXHRComponent.cs
@@ -0,0 +1,146 @@
+﻿using LiveSplit.Model;
+using LiveSplit.TimeFormatters;
+using LiveSplit.UI.Components;
+using LiveSplit.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Xml;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace LiveSplit.DXHR
+{
+    class DXHRComponent : LogicComponent
+    {
+        public override string ComponentName
+        {
+            get { return "DXHR"; }
+        }
+
+        public DXHRSettings Settings { get; set; }
+
+        public bool Disposed { get; private set; }
+        public bool IsLayoutComponent { get; private set; }
+
+        private TimerModel _timer;
+        private GameMemory _gameMemory;
+        private LiveSplitState _state;
+
+        public DXHRComponent(LiveSplitState state, bool isLayoutComponent)
+        {
+            _state = state;
+            this.IsLayoutComponent = isLayoutComponent;
+
+            this.Settings = new DXHRSettings();
+
+           _timer = new TimerModel { CurrentState = state };
+
+            _gameMemory = new GameMemory(this.Settings);
+            _gameMemory.OnFirstLevelLoading += gameMemory_OnFirstLevelLoading;
+            _gameMemory.OnPlayerGainedControl += gameMemory_OnPlayerGainedControl;
+            _gameMemory.OnLoadStarted += gameMemory_OnLoadStarted;
+            _gameMemory.OnLoadFinished += gameMemory_OnLoadFinished;
+            _gameMemory.OnSplitCompleted += gameMemory_OnSplitCompleted;
+            state.OnStart += State_OnStart;
+            _gameMemory.StartMonitoring();
+        }
+
+        public override void Dispose()
+        {
+            this.Disposed = true;
+
+            _state.OnStart -= State_OnStart;
+
+            if (_gameMemory != null)
+            {
+                _gameMemory.Stop();
+            }
+
+        }
+
+        void State_OnStart(object sender, EventArgs e)
+        {
+            _gameMemory.resetSplitStates();
+        }
+
+        void gameMemory_OnFirstLevelLoading(object sender, EventArgs e)
+        {
+            if (this.Settings.AutoStart)
+            {
+                _timer.Reset();
+            }
+        }
+
+        void gameMemory_OnPlayerGainedControl(object sender, EventArgs e)
+        {
+            if (this.Settings.AutoStart)
+            {
+                _timer.Start();
+            }
+        }
+
+        void gameMemory_OnLoadStarted(object sender, EventArgs e)
+        {
+            _state.IsGameTimePaused = true;
+        }
+
+        void gameMemory_OnLoadFinished(object sender, EventArgs e)
+        {
+            _state.IsGameTimePaused = false;
+        }
+
+        void gameMemory_OnSplitCompleted(object sender, GameMemory.SplitArea split, uint frame)
+        {
+            Debug.WriteLineIf(split != GameMemory.SplitArea.None, String.Format("[NoLoads] Trying to split {0} with {1} template, State: {2} - {3}", split, this.Settings.AnyPercentTemplate, _gameMemory.splitStates[(int)split], frame));
+            if (_state.CurrentPhase == TimerPhase.Running && !_gameMemory.splitStates[(int)split] &&
+                ((split == GameMemory.SplitArea.Helgen && this.Settings.Helgen) ||
+                (split == GameMemory.SplitArea.Whiterun && this.Settings.Whiterun) ||
+                (split == GameMemory.SplitArea.ThalmorEmbassy && this.Settings.ThalmorEmbassy) ||
+                (split == GameMemory.SplitArea.Esbern && this.Settings.Esbern) ||
+                (split == GameMemory.SplitArea.Riverwood && this.Settings.Riverwood) ||
+                (split == GameMemory.SplitArea.TheWall && this.Settings.TheWall) ||
+                (split == GameMemory.SplitArea.Septimus && this.Settings.Septimus) ||
+                (split == GameMemory.SplitArea.MzarkTower && this.Settings.MzarkTower) ||
+                (split == GameMemory.SplitArea.ClearSky && this.Settings.ClearSky) ||
+                (split == GameMemory.SplitArea.HorseClimb && this.Settings.HorseClimb) ||
+                (split == GameMemory.SplitArea.CutsceneEnd && this.Settings.CutsceneEnd) ||
+                (split == GameMemory.SplitArea.CutsceneStart && this.Settings.CutsceneStart) ||
+                (split == GameMemory.SplitArea.Alduin1 && this.Settings.Alduin1) ||
+                (split == GameMemory.SplitArea.HighHrothgar && this.Settings.HighHrothgar) ||
+                (split == GameMemory.SplitArea.Solitude && this.Settings.Solitude) ||
+                (split == GameMemory.SplitArea.Windhelm && this.Settings.Windhelm) ||
+                (split == GameMemory.SplitArea.Council && this.Settings.Council) ||
+                (split == GameMemory.SplitArea.Odahviing && this.Settings.Odahviing) ||
+                (split == GameMemory.SplitArea.EnterSovngarde && this.Settings.EnterSovngarde) ||
+                (split == GameMemory.SplitArea.CollegeOfWinterholdQuestlineCompleted && this.Settings.CollegeOfWinterhold) ||
+                (split == GameMemory.SplitArea.CompanionsQuestlineCompleted && this.Settings.Companions) ||
+                (split == GameMemory.SplitArea.DarkBrotherhoodQuestlineCompleted && this.Settings.DarkBrotherhood) ||
+                (split == GameMemory.SplitArea.ThievesGuildQuestlineCompleted && this.Settings.ThievesGuild) ||
+                (split == GameMemory.SplitArea.AlduinDefeated && this.Settings.AlduinDefeated)))
+            {
+                Trace.WriteLine(String.Format("[NoLoads] {0} Split with {2} template - {1}", split, frame, this.Settings.AnyPercentTemplate));
+                _timer.Split();
+                _gameMemory.splitStates[(int)split] = true;
+            }
+        }
+
+        public override XmlNode GetSettings(XmlDocument document)
+        {
+            return this.Settings.GetSettings(document);
+        }
+
+        public override Control GetSettingsControl(LayoutMode mode)
+        {
+            return this.Settings;
+        }
+
+        public override void SetSettings(XmlNode settings)
+        {
+            this.Settings.SetSettings(settings);
+        }
+
+        public override void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode) { }
+        public override void RenameComparison(string oldName, string newName) { }
+    }
+}
---
+++ GameMemory.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiveSplit.DXHR
+{
+    class GameMemory
+    {
+        public enum SplitArea : int
+        {
+            None,
+            SPECIAL,
+            TargetPractice,
+            Escape,
+            Walking,
+            Vault87,
+            Flashbang,
+            Escape2,
+            Walking2,
+            Memorial,
+        }
+
+        public event EventHandler OnFirstLevelLoading;
+        public event EventHandler OnPlayerGainedControl;
+        public event EventHandler OnLoadStarted;
+        public event EventHandler OnLoadFinished;
+        public delegate void SplitCompletedEventHandler(object sender, SplitArea type, uint frame);
+        public event SplitCompletedEventHandler OnSplitCompleted;
+
+        private Task _thread;
+        private CancellationTokenSource _cancelSource;
+        private SynchronizationContext _uiThread;
+        private List<int> _ignorePIDs;
+
+        private DeepPointer _isLoadingPtr;
+        private DeepPointer _streamGroup;
+        private DeepPointer _inGameTime;
+        private DeepPointer _endingCutscene;
+
+        private enum Locations
+        {
+            Tamriel = 0x0000003C,
+            Sovngarde = 0x0002EE41,
+            HelgenKeep01 = 0x0005DE24,
+        }
+
+        private enum ExpectedDllSizes
+        {
+            DXHRSteam = 16166912,
+            DXHRCracked = 0451,
+        }
+
+        public bool[] splitStates { get; set; }
+
+        public void resetSplitStates()
+        {
+            for (int i = 0; i <= (int)SplitArea.Memorial; i++)
+            {
+                splitStates[i] = false;
+            }
+        }
+
+        public GameMemory()
+        {
+            splitStates = new bool[(int)SplitArea.Memorial + 1];
+
+            _isLoadingPtr = new DeepPointer(0x1876708); // == 1 if a loadscreen is happening
+            _streamGroup = new DeepPointer(0x1838D30, 0x4, 0x78, 0x670, 0xEC); // ID of the current stream group
+            _inGameTime = new DeepPointer(0x9E7744, 0x64); // ?
+            _endingCutscene = new DeepPointer(0x1ACE2C0, 0x68); // ?
+
+            resetSplitStates();
+
+            _ignorePIDs = new List<int>();
+        }
+
+        public void StartMonitoring()
+        {
+            if (_thread != null && _thread.Status == TaskStatus.Running)
+            {
+                throw new InvalidOperationException();
+            }
+            if (!(SynchronizationContext.Current is WindowsFormsSynchronizationContext))
+            {
+                throw new InvalidOperationException("SynchronizationContext.Current is not a UI thread.");
+            }
+
+            _uiThread = SynchronizationContext.Current;
+            _cancelSource = new CancellationTokenSource();
+            _thread = Task.Factory.StartNew(MemoryReadThread);
+        }
+
+        public void Stop()
+        {
+            if (_cancelSource == null || _thread == null || _thread.Status != TaskStatus.Running)
+            {
+                return;
+            }
+
+            _cancelSource.Cancel();
+            _thread.Wait();
+        }
+
+        void MemoryReadThread()
+        {
+            Debug.WriteLine("[NoLoads] MemoryReadThread");
+
+            while (!_cancelSource.IsCancellationRequested)
+            {
+                try
+                {
+                    Debug.WriteLine("[NoLoads] Waiting for dxhr.exe...");
+
+                    Process game;
+                    while ((game = GetGameProcess()) == null)
+                    {
+                        Thread.Sleep(250);
+                        if (_cancelSource.IsCancellationRequested)
+                        {
+                            return;
+                        }
+                    }
+
+                    Debug.WriteLine("[NoLoads] Got dxhr.exe!");
+
+                    uint frameCounter = 0;
+
+                    bool prevIsLoading = false;
+
+                    bool loadingStarted = false;
+
+                    while (!game.HasExited)
+                    {
+                        bool isLoading;
+                        _isLoadingPtr.Deref(game, out isLoading);
+
+                        if (isLoading != prevIsLoading)
+                        {
+                            if (isLoading)
+                            {
+                                Debug.WriteLine(String.Format("[NoLoads] Load Start - {0}", frameCounter));
+
+                                loadingStarted = true;
+
+                                // pause game timer
+                                _uiThread.Post(d =>
+                                {
+                                    if (this.OnLoadStarted != null)
+                                    {
+                                        this.OnLoadStarted(this, EventArgs.Empty);
+                                    }
+                                }, null);
+                            }
+                            else
+                            {
+                                Debug.WriteLine(String.Format("[NoLoads] Load End - {0}", frameCounter));
+
+                                if (loadingStarted)
+                                {
+                                    loadingStarted = false;
+
+                                    // unpause game timer
+                                    _uiThread.Post(d =>
+                                    {
+                                        if (this.OnLoadFinished != null)
+                                        {
+                                            this.OnLoadFinished(this, EventArgs.Empty);
+                                        }
+                                    }, null);
+                                }
+                            }
+                        }
+
+                        prevIsLoading = isLoading;
+                        frameCounter++;
+
+                        Thread.Sleep(15);
+
+                        if (_cancelSource.IsCancellationRequested)
+                        {
+                            return;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        Process GetGameProcess()
+        {
+            Process game = Process.GetProcesses().FirstOrDefault(p => p.ProcessName.ToLower() == "fallout3"
+                && !p.HasExited && !_ignorePIDs.Contains(p.Id));
+            if (game == null)
+            {
+                return null;
+            }
+
+            if (game.MainModule.ModuleMemorySize != (int)ExpectedDllSizes.DXHRSteam && game.MainModule.ModuleMemorySize != (int)ExpectedDllSizes.DXHRCracked)
+            {
+                _ignorePIDs.Add(game.Id);
+                _uiThread.Send(d => MessageBox.Show("Unexpected game version. DXHR 1.4.651.0 is required.", "LiveSplit.DXHR",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error), null);
+                return null;
+            }
+
+            return game;
+        }
+    }
+}
